8726f860f0aee7c08f3f726fd4c1a845d855e04d
adinyaev <v.adinyaev5@gmail.com>
2023-04-26 22:58:31 +0300

    EffortController

---
+++ catkin_ws/config_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export ROSCONSOLE_CONFIG_FILE="/home/vodichka/workspace/gibbon/catkin_ws/custom_rosconsole.conf"
+export MESH_WORKSPACE_PATH="/home/vodichka/workspace/gibbon/catkin_ws/src/gibbon/urdf/models"
