20561f9d2a29a7369f4793458512a08d1ca2a7d1
adinyaev <v.adinyaev5@gmail.com>
2023-05-06 14:40:27 +0300

    config

--- catkin_ws/config_env.sh
+++ catkin_ws/config_env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export ROSCONSOLE_CONFIG_FILE="/home/vodichka/workspace/gibbon/catkin_ws/custom_rosconsole.conf"
 export MESH_WORKSPACE_PATH="/home/vodichka/workspace/gibbon/catkin_ws/src/gibbon/urdf/models"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/home/vodichka/Downloads/acados/lib"
+export ACADOS_SOURCE_DIR="/home/vodichka/Downloads/acados"
--- catkin_ws/local_config_acados.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/home/vodichka/Downloads/acados/lib"
-export ACADOS_SOURCE_DIR="/home/vodichka/Downloads/acados"
