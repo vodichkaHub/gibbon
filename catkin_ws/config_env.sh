fe01c150fcbfb0e40a12007aa94db44dd9d6cf9c
adinyaev <v.adinyaev5@gmail.com>
2023-05-14 17:01:57 +0300

    conf gazebo wprld path

--- catkin_ws/config_env.sh
+++ catkin_ws/config_env.sh
@@ -3,3 +3,4 @@ export MESH_WORKSPACE_PATH="/home/vodichka/workspace/gibbon/catkin_ws/src/gibbon
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/home/vodichka/Downloads/acados/lib"
 export ACADOS_SOURCE_DIR="/home/vodichka/Downloads/acados"
 export GAZEBO_MODEL_PATH=/home/vodichka/workspace/gibbon/catkin_ws/src/gibbon/urdf/models
+export GAZEBO_RESOURCE_PATH=/home/vodichka/workspace/gibbon/gazebo
